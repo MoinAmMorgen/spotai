5cebdd647a0d3f4e24e6192b47177c55a39a606d
MoinAmMorgen <moinammorgen@gmail.com>
2024-12-21 21:17:43 +0100

    added close command

--- src/index.js
+++ src/index.js
@@ -1,15 +1,7 @@
-const { app, BrowserWindow } = require ('electron');
+const { app, BrowserWindow, ipcMain } = require('electron');
 const path = require('node:path');
-const started = require('electron-squirrel-startup');
-const { ipcMain } = require('electron');
-
-// Handle creating/removing shortcuts on Windows when installing/uninstalling.
-if (started) {
-  app.quit();
-}
 
 const createWindow = () => {
-  // Create the browser window.
   const win = new BrowserWindow({
     width: 550,
     height: 100,
@@ -19,39 +11,33 @@ const createWindow = () => {
     autoHideMenuBar: true,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
+      contextIsolation: true, 
+      enableRemoteModule: false, 
+      nodeIntegration: false 
     },
   });
 
-  // and load the index.html of the app.
   win.loadFile(path.join(__dirname, 'index.html'));
-
-  // Open the DevTools.
-  win.webContents.openDevTools();
+  win.webContents.openDevTools(); // Open DevTools for debugging
 };
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.whenReady().then(() => {
-  createWindow();
+app.whenReady().then(createWindow);
 
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow();
-    }
-  });
+ipcMain.on('app-quit', () => {
+  console.log('Received app-quit signal. Closing app...');
+  app.quit();
 });
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit();
   }
 });
 
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
 
 // In this file you can include the rest of your app's specific main process
--- src/render.js
+++ src/render.js
@@ -1,6 +1,21 @@
-const close = document.getElementById('search-clear')
-const search = document.getElementById('search-input')
+const close = document.querySelector('.search-clear');
+const search = document.querySelector('.search-input');
 
-close.addEventListener('click', () => {
-    search.value = ''
+if (close) {
+    close.addEventListener('click', () => {
+        search.value = '';
+    });
+}
+search.addEventListener('input', () => {
+        if (search.value.trim() === '@close') {
+            console.log("Closing the app");
+            window.electron.ipcRenderer.send('app-quit');
+        }
+});
+
+search.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+        event.preventDefault(); // Prevent form submission
+        console.log('Custom action for Enter key');
+    }
 });
